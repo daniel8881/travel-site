d398e937983ea2f7b81e66f2932e339d141eecbd
daniel8881 <deepocean@gmail.com>
2016-12-03 22:33:31 -0800

    added browsers support

--- app/gulp/tasks/scripts.js
+++ app/gulp/tasks/scripts.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var config = require('../../../webpack.config');
 
-gulp.task('scripts', function(callback){
+gulp.task('scripts',['modernizr'], function(callback){
   webpack(config, function(err, stats){
     if(err){
       console.log(err.toString());
--- app/gulp/tasks/sprites.js
+++ app/gulp/tasks/sprites.js
@@ -2,10 +2,23 @@ var gulp = require('gulp');
 var svgSprite = require('gulp-svg-sprite');
 var rename = require('gulp-rename');
 var del = require('del');
+var svg2png = require('gulp-svg2png');
 
 var config = {
+  shape: {
+    spacing: {
+      padding: 1
+    }
+  },
   mode: {
     css: {
+      variables: {
+        replaceSvgWithPng: function(){
+          return function(sprite, render){
+            return render(sprite).split('.svg').join('.png');
+          }
+        }
+      },
       sprite: 'sprite.svg',
       render: {
         css: {
@@ -28,8 +41,14 @@ gulp.task('createSprite',['beginClean'], function(){
              .pipe(gulp.dest('./app/temp/sprites/'));
 })
 
-gulp.task('copySvg', ['createSprite'], function(){
-  return gulp.src('./app/temp/sprites/css/**/*.svg')
+gulp.task('createPngCopy',['createSprite'], function(){
+  return gulp.src('./app/temp/sprites/css/*.svg')
+             .pipe(svg2png())
+             .pipe(gulp.dest('./app/temp/sprites/css'));
+})
+
+gulp.task('copySvg', ['createPngCopy'], function(){
+  return gulp.src('./app/temp/sprites/css/**/*.{svg,png}')
              .pipe(gulp.dest('./app/assets/images/sprites'));
 })
 
@@ -43,4 +62,4 @@ gulp.task('endClean', ['copySvg', 'copySprite'], function(){
   return del('./app/temp/sprites');
 })
 
-gulp.task('icons', ['beginClean', 'createSprite', 'copySvg', 'copySprite', 'endClean']);
+gulp.task('icons', ['beginClean', 'createSprite','createPngCopy', 'copySvg', 'copySprite', 'endClean']);
