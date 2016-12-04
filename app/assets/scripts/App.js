4103d570319762fd08c8a0c5141d352a8d22d50a
daniel8881 <deepocean@gmail.com>
2016-12-03 17:14:31 -0800

    completed lazyloading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import MobileMenu from './modules/mobilemenu';
 import RevealOnScroll from './modules/reveal';
 import StickyHeader from './modules/stickyheader';
+import Modal from './modules/Modal';
 import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
 var featureItem = new RevealOnScroll($('.feature-item'), '85%');
 var testimonial = new RevealOnScroll($('.testimonial'), '60%');
 var stickyheader = new StickyHeader();
+var modal = new Modal();
--- app/assets/scripts/modules/stickyheader.js
+++ app/assets/scripts/modules/stickyheader.js
@@ -4,6 +4,7 @@ import smoothScroll from 'jquery-smooth-scroll';
 
 class StickyHeader {
   constructor() {
+    this.lazyImages = $('.lazyload');
     this.siteHeader = $('.site-header');
     this.trigger = $('.large-hero__title');
     this.createHeaderWaypoint();
@@ -11,6 +12,14 @@ class StickyHeader {
     this.headerLinks = $('.primary-nav a');
     this.createPagesectionWaypoint();
     this.addSmoothScrolling();
+    this.refreshWaypoint();
+  }
+
+  refreshWaypoint() {
+    this.lazyImages.on('load', function(){
+      Waypoint.refreshAll();
+    })
+   
   }
 
   createHeaderWaypoint() {
